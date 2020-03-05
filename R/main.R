eae56c8e82b00157573ac75bbf7ea9e45ccc6567
Brittany Belle <bbelle@sonatype.com>
2020-03-05 16:10:13 -0700

    Adds batching and Basic Auth for api call
    YOLO

--- R/main.R
+++ R/main.R
@@ -1,20 +1,23 @@
-"Copyright 2020 Sonatype Inc.
-
-Licensed under the Apache License, Version 2.0 (the \"License\");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an \"AS IS\" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License."
+# Copyright 2020 Sonatype Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License."
 
 library("httr")
 library("rjson")
 
+#' Collects dependencies installed locally, and turns them into purls
+#'
+#' @return A list of purls
 collect_dependencies_and_turn_into_purls <- function() {
   ip = as.data.frame(installed.packages()[,c(1,3:4)])
   ip = ip[is.na(ip$Priority),0:2,drop=FALSE]
@@ -30,17 +33,64 @@ collect_dependencies_and_turn_into_purls <- function() {
   return(purls)
 }
 
-call_oss_index <- function(purls) {
-  OSS_INDEX_URL <- "https://ossindex.sonatype.org/api/v3/component-report"
+OSS_INDEX_URL <- "https://ossindex.sonatype.org/api/v3/component-report"
+MAX_NUMBER_OF_PURLS_PER_API_CALL <- 128
 
-  BODY <- list(coordinates = purls)
+#' Calls OSS Index given a list of purls
+#'
+#' @param allPurls A list of purls
+#' @return A list of results from OSS Index
+call_oss_index <- function(allPurls) {
+  batchesOfPurls <- .batch_purls(allPurls)
 
-  r <- httr::POST(OSS_INDEX_URL, body = BODY, encode = "json")
-
-  result <- rjson::fromJSON(httr::content(r, "text", encoding="UTF-8"))
+  result <- list()
+  for (batch in batchesOfPurls) {
+    BODY <- list(coordinates = batch)
+    r <- httr::POST(OSS_INDEX_URL, body = BODY, encode = "json", httr::authenticate("bbelle@sonatype.com", "API_TOKEN_HERE", type = "basic"))
+    tryCatch({
+      batchResult <- rjson::fromJSON(httr::content(r, "text", encoding="UTF-8"))
+      result <- c(result, batchResult)
+    }, warning = function(w) {
+      print(w)
+    }, error = function(e) {
+      print(e)
+    }, finally = {
+      # NO OP
+    })
+  }
   return(result)
 }
 
+# Returns list of "batches". Each batch is a list of purls, with a max length of MAX_NUMBER_OF_PURLS_PER_API_CALL.
+.batch_purls <- function(purls) {
+  maxBatchSize = MAX_NUMBER_OF_PURLS_PER_API_CALL
+  numberOfPurls <- length(purls)
+  numberOfBatches <- .get_number_of_batches(numberOfPurls, maxBatchSize)
+  remainder <- numberOfPurls %% maxBatchSize
+
+  batches <- vector(mode="list")
+  for (i in seq(numberOfBatches)) {
+    startOfRange = 1 + maxBatchSize * (i-1)
+    endOfRange = maxBatchSize + maxBatchSize * (i-1)
+    if (i == numberOfBatches) {
+      endOfRange = endOfRange - maxBatchSize + remainder
+    }
+    batches <- append(batches, list(purls[startOfRange:endOfRange]))
+  }
+  return(batches)
+}
+
+.get_number_of_batches <- function(numberOfPurls, maxBatchSize) {
+  numberOfBatches <- numberOfPurls %/% maxBatchSize
+  if (numberOfPurls %% maxBatchSize > 0) {
+    numberOfBatches <- numberOfBatches + 1
+  }
+  return(numberOfBatches)
+}
+
+#' Audits a response from OSS Index and prints out a software bill of materials
+#'
+#' @param response A list of coordinates and vulnerabilities from OSS Index
 audit_response_from_oss_index <- function(response) {
   vulnerableComponents <- .extract_vulnerable_components(response)
 
@@ -143,8 +193,8 @@ audit_response_from_oss_index <- function(response) {
 
 audit_deps_with_oss_index <- function() {
   purls <- collect_dependencies_and_turn_into_purls()
-  res <- call_oss_index(purls)
-  audit_response_from_oss_index(res)
+  results <- call_oss_index(purls)
+  audit_response_from_oss_index(results)
 }
 
 audit_deps_with_oss_index()
