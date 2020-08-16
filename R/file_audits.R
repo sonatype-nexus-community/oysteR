a2c85132cfae0056ecbc55f3dd09d23a62df87c7
josiahparry <josiah.parry@gmail.com>
2020-08-15 23:59:05 -0400

    create functions for auditing requirements.txt and renv.lock files

--- R/file_audits.R
+++ R/file_audits.R
@@ -1,28 +1,4 @@
-#' Audit a requirements.txt file
-#'
-#' This function searches the OSS index for vulnerabilities recorded for packages listed in a requirements.txt file based on PyPi.
-#'
-#' pip is a standard of python package management based on the Python Package Index (PyPI). pip uses a requirements.txt file to manage of Python libraries. The requirements.txt file contains package names and versions (often used to manage a virtual environment).
-#'
-#' @param requirements The file path of a requirements.txt file.
-#' @param verbose Default \code{TRUE}.
-#'
-#' @importFrom dplyr %>% mutate
-#' @importFrom purrr map_dfr
-#' @importFrom tibble as_tibble
-#' @export
-
-audit_req_txt <- function(requirements, verbose = TRUE) {
-
-  readLines(requirements) %>%
-    strsplit(">=|==|>") %>%
-    map_dfr(~data.frame(package = .x[1], version = .x[2])) %>%
-    mutate(audit_pkgs(package, version, schema = "pypi", verbose = verbose)) %>%
-    as_tibble()
-
-}
-
-#' Audit an renv.lock file
+#' Audit an renv.lock file.
 #'
 #' This function searches the OSS index for vulnerabilities recorded for packages listed in an renv.lock file.
 #'
@@ -49,3 +25,30 @@ audit_renv_lock <- function(lockfile, verbose = TRUE) {
     as_tibble()
 
 }
+
+#' Audit a requirements.txt file.
+#'
+#' This function searches the OSS index for vulnerabilities recorded for packages listed in a requirements.txt file based on PyPi.
+#'
+#' pip is a standard of python package management based on the Python Package Index (PyPI). pip uses a requirements.txt file to manage of Python libraries. The requirements.txt file contains package names and versions (often used to manage a virtual environment).
+#'
+#' @param requirements The file path of a requirements.txt file.
+#' @param verbose Default \code{TRUE}.
+#'
+#' @importFrom dplyr %>% mutate
+#' @importFrom purrr map_dfr
+#' @importFrom tibble as_tibble
+#' @export
+
+audit_req_txt <- function(requirements, verbose = TRUE) {
+
+  readLines(requirements) %>%
+    strsplit(">=|==|>") %>%
+    map_dfr(~data.frame(package = .x[1], version = .x[2])) %>%
+    mutate(audit_pkgs(package, version, schema = "pypi", verbose = verbose)) %>%
+    as_tibble()
+
+}
+
+
+# TO DO: environment.yml for Conda
