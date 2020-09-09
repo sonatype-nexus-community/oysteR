72fa991b9b413131bd4bfe31c09bd1c0f979b193
Colin Gillespie <csgillespie@gmail.com>
2020-09-09 22:49:22 +0100

    Adding initial tests suits for file parsing

---
+++ tests/testthat/test-renv.R
@@ -0,0 +1,11 @@
+test_that("Test Renvlock file", {
+  skip_on_cran()
+
+  # Sanity check to make sure the directory exists
+  expect_true(file.exists("renv.lock"))
+  audit = oysteR::audit_renv_lock()
+  expect_equal(ncol(audit), 7)
+  expect_equal(nrow(audit), 57)
+})
+
+
