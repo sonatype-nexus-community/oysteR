4ad12de9226e45a787c94b01eda71c1fdc50552f
Colin Gillespie <csgillespie@gmail.com>
2020-12-15 18:49:10 +0000

    Built site for oysteR: 0.0.3.9009@

--- tests/testthat/test-requirements.R
+++
@@ -1,9 +0,0 @@
-test_that("Test requirements file", {
-  skip_on_cran()
-
-  # Sanity check to make sure the directory exists
-  expect_true(file.exists("requirements.txt"))
-  audit = audit_req_txt()
-  expect_equal(ncol(audit), 8)
-  expect_equal(nrow(audit), 31)
-})
--- tests/testthat/test-secure.R
+++
@@ -1,5 +0,0 @@
-test_that("Test expect_secure", {
-  skip_on_cran()
-  ## Tests function and this package
-  oysteR::expect_secure("oysteR")
-})
