817ef3a7e9a9b8f0e0690aa32c82bb9b968a74ac
Colin Gillespie <csgillespie@gmail.com>
2021-02-26 15:00:17 +0000

    A little refactoring of @JosiahParry's PR

--- tests/testthat/test-env-yml.R
+++ tests/testthat/test-env-yml.R
@@ -3,9 +3,11 @@ test_that("Test environment.yml file", {
 
   # Sanity check to make sure the directory exists
   expect_true(file.exists("environment.yml"))
-  audit = oysteR::audit_env_yml()
+  audit = oysteR::audit_conda("tests/testthat/")
   expect_equal(ncol(audit), 8)
   expect_equal(nrow(audit), 73)
-  # ## Run when no renv lock available
-  expect_error(oysteR::audit_env_yml("../"))
+  expect_gte(sum(audit$no_of_vulnerabilities), 95) # This could increase
+
+  # ## Run when no file available
+  expect_error(oysteR::audit_conda(fname = "file-does-not-exist"))
 })
