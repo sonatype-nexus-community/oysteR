e63bc4eb2c6d501372e93bcc1f154f55727e8913
Colin Gillespie <csgillespie@gmail.com>
2021-05-22 20:18:14 +0100

    Use xml2 for SBOM

--- R/sbom.R
+++ R/sbom.R
@@ -12,34 +12,88 @@
 # See the License for the specific language governing permissions and
 # limitations under the License."
 
-library(XML)
+# Helper function: adds a node!
+add_child_node = function(parent, node_name, node_text = NULL) {
+  new_node = xml2::xml_new_root(node_name)
+  if (!is.null(node_text)) xml2::xml_text(new_node) = node_text
+  xml2::xml_add_child(parent, new_node)
+}
 
-create_sbom = function(purls, verbose) {
+# Header text. Only thing that varies is package version
+get_root_node = function() {
+  root = xml2::xml_new_root("bom", xmlns = "http://cyclonedx.org/schema/bom/1.3")
+  metadata = add_child_node(root, "metadata")
+  tools = add_child_node(metadata, "tools")
+  add_child_node(tools, "tool")
+  add_child_node(tools, "vendor", node_text = "sonatype-nexus-community")
+  add_child_node(tools, "name", node_text = "oysteR")
+  add_child_node(tools, "version", node_text = as.character(packageVersion("oysteR")))
+  root
+}
+
+# Creates a purl XML component
+get_component = function(purl) {
+  purl_split = stringr::str_match(purl, pattern = "pkg:(.+)@(.+)")
+  purl_name = purl_split[1, 2]
+  purl_version = purl_split[1, 3]
+  component = xml2::xml_new_root("component", type = "library", "bom-ref" = purl)
+  add_child_node(component, "name", purl_name)
+  add_child_node(component, "version", purl_version)
+  add_child_node(component, "purl", purl)
+  component
+}
+
+create_sbom = function(purls, verbose = TRUE) {
   if (isTRUE(verbose)) {
     cli::cli_alert("Beginning to create CycloneDX SBOM")
   }
 
-  bom = newXMLNode("bom", attrs=c(xmlns='http://cyclonedx.org/schema/bom/1.3'))
-  metadata = newXMLNode("metadata", parent=bom)
-  tools = newXMLNode("tools", parent=metadata)
-  tool = newXMLNode("tool", parent=tools)
-  newXMLNode("vendor", "sonatype-nexus-community", parent=tool)
-  newXMLNode("name", "oysteR", parent=tool)
-  # Need to obtain the oysteR version for this
-  newXMLNode("version", "oysteR-Version", parent=tool)
-
-  components = newXMLNode("components", parent=bom)
+  root = get_root_node()
+  components = xml2::xml_add_child(root, "components")
   for (purl in purls) {
-    component = newXMLNode("component", attrs=c(type='library', 'bom-ref'=purl), parent=components)
-    # Need to split the PURL to have the name/version for use here
-    newXMLNode("name", "name-value", parent=component)
-    newXMLNode("version", "version-value", parent=component)
-    newXMLNode("purl", purl, parent=component)
+    component = get_component(purl)
+    add_child_node(components, component)
   }
-
   if (isTRUE(verbose)) {
     cli::cli_alert("Finished with CycloneDX SBOM")
   }
 
-  bom
+  #cat(as.character(root))
+  return(root)
 }
+
+
+##############################
+# To delete before merging
+# Uses XML
+
+# create_sbom = function(purls, verbose) {
+#   library(XML)
+#   if (isTRUE(verbose)) {
+#     cli::cli_alert("Beginning to create CycloneDX SBOM")
+#   }
+#
+#   bom = newXMLNode("bom", attrs=c(xmlns='http://cyclonedx.org/schema/bom/1.3'))
+#   metadata = newXMLNode("metadata", parent=bom)
+#   tools = newXMLNode("tools", parent=metadata)
+#   tool = newXMLNode("tool", parent=tools)
+#   newXMLNode("vendor", "sonatype-nexus-community", parent=tool)
+#   newXMLNode("name", "oysteR", parent=tool)
+#   # Need to obtain the oysteR version for this
+#   newXMLNode("version", "oysteR-Version", parent=tool)
+#
+#   components = newXMLNode("components", parent=bom)
+#   for (purl in purls) {
+#     component = newXMLNode("component", attrs=c(type='library', 'bom-ref'=purl), parent=components)
+#     # Need to split the PURL to have the name/version for use here
+#     newXMLNode("name", "name-value", parent=component)
+#     newXMLNode("version", "version-value", parent=component)
+#     newXMLNode("purl", purl, parent=component)
+#   }
+#
+#   if (isTRUE(verbose)) {
+#     cli::cli_alert("Finished with CycloneDX SBOM")
+#   }
+#
+#   bom
+# }
